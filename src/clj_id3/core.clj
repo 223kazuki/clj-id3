b29a86884a98957a4f3b1fdb93b86edb8a6b7cab
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2019-02-21 22:25:44 -0800

    Update.

--- src/clj_id3/core.clj
+++ src/clj_id3/core.clj
@@ -7,42 +7,49 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn normalize-str [str]
-  (Normalizer/normalize str java.text.Normalizer$Form/NFKC))
+(defn normalize-str [string]
+  (-> string
+      (Normalizer/normalize java.text.Normalizer$Form/NFKC)))
 
 (defn normalize-id3 [data]
   (let [{:keys [mp3 file
                 track artist title album original-artist album-artist
-                year genre genre-description original-artist album-artist album-image album-image-mime-type]} data]
+                year genre genre-description original-artist album-artist
+                part-of-set album-image album-image-mime-type]} data]
     (if-not (and track title album (or artist album-artist))
-      (println file " doesn't have enough tags.:" track artist title album album-artist)
-      (let [id3 (ID3v24Tag.)
-            data (cond-> data
-                   artist (#(let [artist (normalize-str artist)]
-                              (.setArtist id3 artist)
-                              (assoc % :artist artist)))
-                   title (#(let [title (normalize-str title)]
-                             (.setTitle id3 title)
-                             (assoc % :title title)))
-                   album (#(let [album (normalize-str album)]
-                             (.setAlbum id3 album)
-                             (assoc % :album album)))
-                   original-artist (#(let [original-artist (normalize-str original-artist)]
-                                       (.setOriginalArtist id3 original-artist)
-                                       (assoc % :original-artist original-artist)))
-                   album-artist (#(let [album-artist (normalize-str album-artist)]
-                                    (.setAlbumArtist id3 album-artist)
-                                    (assoc % :album-artist album-artist))))]
-        (when track (.setTrack id3 track))
-        (when year (.setYear id3 year))
-        (when genre (.setGenre id3 genre))
-        (when genre-description (.setGenreDescription id3 genre-description))
-        (when original-artist (.setOriginalArtist id3 original-artist))
-        (when album-artist (.setAlbumArtist id3 album-artist))
-        (when (and album-image album-image-mime-type)
-          (.setAlbumImage id3 album-image album-image-mime-type))
-        (.setId3v2Tag mp3 id3)
-        data))))
+      (println file "doesn't have enough tags.:" track artist title album album-artist)
+      (try
+        (let [id3 (ID3v24Tag.)
+              data (cond-> data
+                     artist (#(let [artist (normalize-str artist)]
+                                (.setArtist id3 artist)
+                                (assoc % :artist artist)))
+                     title (#(let [title (normalize-str title)]
+                               (.setTitle id3 title)
+                               (assoc % :title title)))
+                     album (#(let [album (normalize-str album)]
+                               (.setAlbum id3 album)
+                               (assoc % :album album)))
+                     original-artist (#(let [original-artist (normalize-str original-artist)]
+                                         (.setOriginalArtist id3 original-artist)
+                                         (assoc % :original-artist original-artist)))
+                     album-artist (#(let [album-artist (normalize-str album-artist)]
+                                      (.setAlbumArtist id3 album-artist)
+                                      (assoc % :album-artist album-artist))))]
+          (when track (.setTrack id3 track))
+          (when year (.setYear id3 year))
+          (when genre (.setGenre id3 genre))
+          (when genre-description (.setGenreDescription id3 genre-description))
+          (when part-of-set (.setPartOfSet id3 part-of-set))
+          (when original-artist (.setOriginalArtist id3 original-artist))
+          (when album-artist (.setAlbumArtist id3 album-artist))
+          (when (and album-image album-image-mime-type)
+            (.setAlbumImage id3 album-image album-image-mime-type))
+          (.setId3v2Tag mp3 id3)
+          data)
+        (catch Exception e
+          (println file "throw erros in creating tag.")
+          (.printStackTrace e))))))
 
 (defn get-id3 [file]
   (try
@@ -68,43 +75,55 @@
           (.getUrl id3) (assoc :url (.getUrl id3))
           (.getEncoder id3) (assoc :encoder (.getEncoder id3))
           (.getAlbumImage id3) (assoc :album-image (.getAlbumImage id3))
+          (.getPartOfSet id3) (assoc :part-of-set (.getPartOfSet id3))
           (.getAlbumImageMimeType id3) (assoc :album-image-mime-type (.getAlbumImageMimeType id3)))
         {:file file}))
     (catch Exception e
-      (println file " throw erros.")
+      (println file "throw erros.")
       (.printStackTrace e))))
+
+(defn escape-name [str]
+  (str/replace str #"/|／" "\\\\"))
 
 (defn save-mp3 [data]
   (let [{:keys [mp3 id3 file
-                track artist title album album-artist]} data
+                track artist title album album-artist part-of-set]} data
         out (io/file "output"
-                     (or album-artist artist)
-                     album
-                     (format "%02d %s.mp3"
-                             (-> track
-                                 (str/split #"/")
-                                 first
-                                 Integer/parseInt) title))]
+                     (escape-name (or album-artist artist))
+                     (escape-name album)
+                     (str
+                      (when part-of-set
+                        (-> part-of-set
+                            (str/split #"/")
+                            first
+                            (str "-")))
+                      (format "%02d %s.mp3"
+                              (-> track
+                                  (str/split #"/")
+                                  first
+                                  Integer/parseInt)
+                              (escape-name title))))]
     (io/make-parents out)
     (try
-      (when (.exists out) (println (.getAbsolutePath out) " exists."))
-      (.save mp3 (.getAbsolutePath out))
+      (if (.exists out)
+        (println (.getAbsolutePath out) "exists.")
+        (.save mp3 (.getAbsolutePath out)))
       (catch Exception e
-        (println file " failed to save.")
+        (println file "failed to save.")
         (.printStackTrace e)))))
 
-(defn -main
+(defn -main`
   "I don't do a whole lot ... yet."
   [& args]
-  (->> (io/file "./input")
+  (->> (io/file "/work/Music")
        (file-seq)
        (filter #(.isFile %))
        (map #(.getAbsolutePath %))
        (filter #(or (str/ends-with? % ".mp3")
-                    (println % " is not music file.")))
+                    (println % "is not music file.")))
        (map get-id3)
        (filter #(or (:id3 %)
-                    (println % " doesn't have id3 tag.")))
+                    (println % "doesn't have id3 tag.")))
        (map normalize-id3)
        (filter identity)
        (map save-mp3)
